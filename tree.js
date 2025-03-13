5fda8189fb796475cb7be7e3d77a041610c5a8fe
shinjir0u <htooagwai9@gmail.com>
2025-03-13 15:51:21 +0630

    Add insert, delete and find functions

--- tree.js
+++ tree.js
@@ -28,6 +28,64 @@ class Tree {
     return root;
   }
 
+  insert(value) {
+    let currentNode = this.root;
+    let previousNode;
+    let direction;
+
+    while (currentNode) {
+      if (value === currentNode.data) return;
+      else if (value < currentNode.data)
+        [previousNode, currentNode, direction] = [
+          currentNode,
+          currentNode.left,
+          "left",
+        ];
+      else if (value > currentNode.data)
+        [previousNode, currentNode, direction] = [
+          currentNode,
+          currentNode.right,
+          "right",
+        ];
+    }
+    previousNode[direction] = new Node(value);
+  }
+
+  delete(value) {
+    let currentNode = this.root;
+    let previousNode;
+    let direction;
+
+    while (currentNode) {
+      if (value === currentNode.data) break;
+      else if (value < currentNode.data)
+        [previousNode, currentNode, direction] = [
+          currentNode,
+          currentNode.left,
+          "left",
+        ];
+      else if (value > currentNode.data)
+        [previousNode, currentNode, direction] = [
+          currentNode,
+          currentNode.right,
+          "right",
+        ];
+    }
+    if (currentNode === null) return;
+    previousNode[direction] = currentNode.left || currentNode.right;
+    if (currentNode.left !== null) currentNode.left.right = currentNode.right;
+  }
+
+  find(value) {
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value === currentNode.data) return currentNode;
+      else if (value < currentNode.data) currentNode = currentNode.left;
+      else if (value > currentNode.data) currentNode = currentNode.right;
+    }
+    return currentNode;
+  }
+
   prettyPrint(node, prefix = "", isLeft = true) {
     if (node === null) {
       return;
@@ -48,6 +106,5 @@ class Tree {
 
 const tree = new Tree([6, 2, 7, 8, 1, 9, 6, 0]);
 console.log(tree.array);
-tree.prettyPrint(tree.root);
-tree.insert(4);
+console.log(tree.find(6));
 tree.prettyPrint(tree.root);
